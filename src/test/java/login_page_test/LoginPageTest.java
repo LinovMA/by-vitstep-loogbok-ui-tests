c42ab4d33f397b65dc6a5da7816070e3ac964705
гк <@adamant.local>
2023-02-06 20:10:40 +0300

    Class work 06/02/2023

---
+++ src/test/java/login_page_test/LoginPageTest.java
@@ -0,0 +1,22 @@
+package login_page_test;
+
+import base.BaseUITestsClass;
+import org.junit.Test;
+
+public class LoginPageTest extends BaseUITestsClass {
+
+    @Test
+    public void verifyLoginInputVisibility(){
+
+    }
+
+    @Test
+    public void verifyLoginInputParameters(){
+
+    }
+
+    @Test
+    public void verifyLoginInputInvalidParameters(){
+
+    }
+}
